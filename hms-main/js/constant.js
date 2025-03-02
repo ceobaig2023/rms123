69cc2429dede2d2afc3c78e2d6068de3b4428dff
ceobaig2023 <ceobaig2023@gmail.com>
2025-03-03 00:14:39 +0530

    Update constant.js

--- hms-main/js/constant.js
+++ hms-main/js/constant.js
@@ -1,4 +1,1 @@
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxEOmlAzX9BPSM03rkpT8a4zY96mMaBQkpy5cDXBKQVf7oW5zFNVmaKowgER2r_2IAc5Q/exec';
-//const CITY_DROPDOWN =["ALL","CITY 1 ","CITY 2 "];
-//const CITY=["Mecca","Medinah"]
 
